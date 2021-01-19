120d0f61914dd1596c6bf5fa5df57c52e2a538ea
unknown <j0h@example.com>
2021-01-19 17:01:30 +0200

    set up vuejs and first element creating

---
+++ app.js
@@ -0,0 +1,7 @@
+let app = new Vue({                    //define new object
+    el: '#app',                        //define element and define which element, #for access
+    data:{                            //define data property
+       // name: 'Easy learning'         //defina name 
+        message: 'hello friends'        //define mesage
+    }
+})
