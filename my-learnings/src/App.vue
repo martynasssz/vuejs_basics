a1279469ddc15939b41644e1940deb773582809c
unknown <j0h@example.com>
2021-01-22 08:27:18 +0200

    VueJs Nested Components: addint to app.vue import, components, styles and showing in template

--- my-learnings/src/App.vue
+++ my-learnings/src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
     <h2> {{title}} </h2>
+    <Navbar></Navbar> <!-- to display navbar-->
+    <!-- <sidebar></sidebar> to display sidebar-->
   </div>
 </template>
 
 <script>
 
+import Navbar from './components/Navbar.vue' //define component included in root component
+//import sidebar from './components/Sidebar.vue'
 export default {
   name: 'App',  //one property
     data(){     //data is method
       return {
-        title: 'Vue vebsite is running' //title is object with assigned value
+        title: 'Vue website is running' //title is object with assigned value
       }
+    },
+    components:{
+      Navbar, //Sidebar   //defined two components Navbar and Sidebar
     }
  
   
