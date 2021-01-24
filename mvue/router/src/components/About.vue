9fa7b4a85a4999463469a4d1d2cfa628625d76a5
unknown <j0h@example.com>
2021-01-24 20:21:47 +0200

    VueJs Setting Routes: created 3 new pages home, about, contact and created routes for them

---
+++ mvue/router/src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">    
+    <h2>About page</h2>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ mvue/router/src/components/Contact.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">    
+    <h2>Contact page</h2>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
