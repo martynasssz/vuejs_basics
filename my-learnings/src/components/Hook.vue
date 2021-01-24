3ba15aa78b0d49b015be12642a701eb40fa19dfa
unknown <j0h@example.com>
2021-01-24 16:48:49 +0200

    VueJS Axios Request: IMPORTANT lesson how to take data from json url: jsonplaceholder.typicode.com/post and show in out websit. Similar we can with database data

--- my-learnings/src/components/Hook.vue
+++ my-learnings/src/components/Hook.vue
@@ -1,44 +1,44 @@
 <template>
     <div class="Hook">
-        <h1> {{ title }} </h1> <!-- pass title to property-->
-        <button @click="ChangeTitle">Click Me</button> <!-- after click my will be update name form "Hook main data" to "Easy learning"--> 
+        <h1> Posts </h1><hr> <!-- pass title to property-->
+        <div v-for="post in posts" :key="post.id"> <!-- :key we want to get id using :key-->
+            <h2> {{post.title}}</h2> <!-- we display post title-->
+            <p>{{post.body}}</p> <!-- we display body-->
+            <p>{{post.body}}</p>
+        </div>
+
+       
     </div>    
 </template>
 
 <script>
+import axios from 'axios' //inpor jsonplaceholder
 export default {
    name: 'Hook',
    data(){
        return { 
-          title: 'Hook Main data'          //pass property title
+           posts:[] 
        }
    },
    methods: {
-      ChangeTitle(){
-          this.title = 'Easy learning' 
-      } 
+     
    },
-//    beforeMount (){
-//        alert('Mount') ////show alert after created 
-//    },
-
-   beforeCreate() {  //function from hooks
-   alert('Before created hook')   //show alert firstly
-   },
-   created (){
-       alert('Created hook') ////show alert after created after that page will be loaded
-   },
-   beforeUpdate(){
-       alert("Before update massage") 
+   created(){  //created hook
+        axios.get('https://jsonplaceholder.typicode.com/posts') // if we get any data, will work then() else catch()
+        .then(response => {
+            this.posts = response.data  //we put everything from response data to post:[] array
+        })
+        .catch(error => {  //if we have no any data error will be showed
+            console.log(error)
+        })
    }
-
-
-
-
 }
 </script>
 
 <style>  
-   
+h1{
+    color: red;
+    text-align: center;
+}
 
 </style>
