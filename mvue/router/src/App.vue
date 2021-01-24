36c7c51ffb7698c0ce8a3ccc2f072658c103d911
unknown <j0h@example.com>
2021-01-24 20:50:34 +0200

    VueJs Router link: learned haw to crrate links. IMPORTANT!

--- mvue/router/src/App.vue
+++ mvue/router/src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <Navbar></Navbar> <!--load navbar-->
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
+
 export default {
-  name: 'App'
+  components: { Navbar },
+  name: 'App',
+  compnents: {
+    Navbar
+  }
+
+
 }
 </script>
 
